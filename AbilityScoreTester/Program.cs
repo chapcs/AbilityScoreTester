220989de0bda88dd9fde26edea09d09440c8c20d
cozz-py <bencozzens13@gmail.com>
2023-02-17 08:11:45 -0500

    Comments added for ReadInt

--- AbilityScoreTester/Program.cs
+++ AbilityScoreTester/Program.cs
@@ -34,14 +34,17 @@ internal class Program
     /// <returns>int value read, or default if unable to parse</returns>
     static int ReadInt(int lastUsedValue, string prompt)
     {
-
+        // write prompt followed by [default value]:
+        // Read the line from the input and use int.TryParse to attempt to parse it
+        // If it can be parsed, write "using value" + value to console
+        // Otherwise write "using default value" + lastUsedValue to the console
     }
     /// <summary>
     /// Parses like ReadInt but for doubles
     /// </summary>
     static double ReadDouble(double lastUsedValue, string prompt)
     {
-
+        
     }
     private static void Main(string[] args)
     {
